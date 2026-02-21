e730d5a075207db7ab56081fcca128ccb0400de7
Simon Bedoya <simon.bedoyau@upb.edu.co>
2026-02-21 16:54:41 -0500

    first commit

---
+++ Backend/optimizacionespacios/src/main/java/com/tic/optimizacionespacios/OptimizacionespaciosApplication.java
@@ -0,0 +1,13 @@
+package com.tic.optimizacionespacios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizacionespaciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizacionespaciosApplication.class, args);
+	}
+
+}
---
+++ Backend/optimizacionespacios/src/test/java/com/tic/optimizacionespacios/OptimizacionespaciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tic.optimizacionespacios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizacionespaciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
